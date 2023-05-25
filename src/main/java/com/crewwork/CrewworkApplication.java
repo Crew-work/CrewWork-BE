437f5e319ac7632be7d73c6e9946dc938940bba3
skatn <87296704+skatn@users.noreply.github.com>
2023-05-25 19:04:14 +0900

    Project Upload

---
+++ src/main/java/com/crewwork/CrewworkApplication.java
@@ -0,0 +1,13 @@
+package com.crewwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewworkApplication.class, args);
+	}
+
+}
